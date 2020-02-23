e6f3bc270b930482a2e002cb4eb5c5a34b6a7065
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 00:38:17 +0530

    creating the main method and creating the ContactList object and all the fields required

---
+++ untitled/src/contact/ContactList.java
@@ -0,0 +1,26 @@
+package contact;
+
+import adt.ContactADT;
+
+public class ContactList implements ContactADT {
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        return false;
+    }
+
+    @Override
+    public void search(String name) {
+
+    }
+
+    @Override
+    public void viewAll() {
+
+    }
+}
---
+++ untitled/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import contact.ContactList;
+import contact.Person;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ContactList contactList = new ContactList();
+        String FirstName;
+        String LastName;
+        String[] contactNumber = new String[3];
+        String email;
+        boolean flag;
+
+    }
+}
