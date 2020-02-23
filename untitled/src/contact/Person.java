d23592ff9410b40d266dd22e280720303477141e
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 00:22:47 +0530

    making amends in the toString method as required by the program

--- untitled/src/contact/Person.java
+++ untitled/src/contact/Person.java
@@ -10,12 +10,16 @@ public class Person {
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("Person{");
-        sb.append("firstName='").append(firstName).append('\'');
-        sb.append(", lastName='").append(lastName).append('\'');
-        sb.append(", contactNo=").append(contactNo == null ? "null" : Arrays.asList(contactNo).toString());
-        sb.append(", email='").append(email).append('\'');
-        sb.append('}');
+        final StringBuffer sb = new StringBuffer("-------- * -------- * -------- * --------\n");
+        sb.append("First Name: ").append(firstName).append('\n');
+        sb.append("Last Name: ").append(lastName).append('\n');
+        sb.append("Contact No: ");
+        for (int i = 0; i < contactNo.length; i++) {
+            sb.append((contactNo[0] != null && i == 0 ? contactNo[0] : "") + (contactNo[1] != null && i == 1 ? ", " + contactNo[1] : "") +
+                    (contactNo[2] != null && i == 2 ? ", " + contactNo[i] : ""));
+        }
+        sb.append("\nEmail: ").append(email).append('\n');
+        sb.append("-------- * -------- * -------- * --------\n");
         return sb.toString();
     }
 }
