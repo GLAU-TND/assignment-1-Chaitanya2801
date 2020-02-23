15c73581bba3fae5cdc693c0dd387d7fcaaac2a9
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 00:25:31 +0530

    creating getters and setters of the private field mentioned in the program

--- untitled/src/contact/Person.java
+++ untitled/src/contact/Person.java
@@ -8,6 +8,38 @@ public class Person {
     private String[] contactNo;
     private String email;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String[] getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String[] contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Person(String firstName, String lastName, String[] contactNo, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
